b086950157dd1a96e4bff880f8290b04a9fd7be8
Gustavo Ferreira <gfmb94@gmail.com>
2024-07-13 13:15:19 -0300

    Tipos de comentarios

---
+++ java-terminal/src/Comentarios.java
@@ -0,0 +1,22 @@
+public class Comentarios {
+    public static void main(String [] args){
+        //Olá, eu sou um comentário em uma única 
+
+        /*
+         * Olá 
+         * Eu sou um comentário mais detalhado
+         * quando necessario
+         */
+    }
+     
+    /**
+     * Estas duas estrelinhas acima
+     * é para identificar que você
+     * pretende elaborar um documentario
+     * a nível de documentação.
+     * Que incrivel!
+     */
+    public void metodo(){
+        
+    }
+}
