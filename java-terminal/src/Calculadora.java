fd95ece33676ea85dd0909d4057e67bc97adecd5
Gustavo Ferreira <gfmb94@gmail.com>
2024-07-13 13:01:01 -0300

    Implementando a documentação do código

---
+++ java-terminal/src/Calculadora.java
@@ -0,0 +1,24 @@
+/**
+ * <h1>Calculadora</h1>
+ * A Calculadora realiza operações matematicas entre número inteiros
+ * <p>
+ * <b>Note:</b> Leia atentamente a documentação dessas classes
+ * para desfrutar dos recursos oferecidos pelo autor
+ * 
+ * @author: Gustavo Ferreira
+ * @version: 1.0
+ * @since: 13/07/2024
+ */
+public class Calculadora {
+    /**
+     * Este método é utilizado para somar dois números inteiros
+     * @param numeroUm este é o primeiro número do método
+     * @param numeroDois este é o segundo número do método
+     * @return int o resultado deste método é a soma dos dois números
+     */
+
+     public int somar(int numeroUm,int numeroDois){
+        return numeroUm+numeroDois;
+     }
+    
+}
