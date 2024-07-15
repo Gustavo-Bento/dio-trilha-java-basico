c37aa0facf016f051f116fa42679fd3e8eb00ef4
Gustavo Ferreira <gfmb94@gmail.com>
2024-07-14 23:55:22 -0300

    break e continue

---
+++ controle-fluxo/ExemploBreakContinue.java
@@ -0,0 +1,9 @@
+public class ExemploBreakContinue {
+    public static void main(String[] args) {
+        for(int x = 1;x<=5; x++){
+            if(x==4)
+                continue;
+            System.out.println(x);
+        }
+    }
+}
