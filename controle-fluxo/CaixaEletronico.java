f751bee7ce478e4d9e1e3e66b75e21c6670ef523
Gustavo Ferreira <gfmb94@gmail.com>
2024-07-14 23:01:30 -0300

    condicional composta

--- controle-fluxo/CaixaEletronico.java
+++ controle-fluxo/CaixaEletronico.java
@@ -1,11 +1,13 @@
 public class CaixaEletronico {
     public static void main(String[] args) {
-        double saldo = 25.0;
+        double saldo = 16.0;
         double valorSolitado = 17.0;
 
         if(valorSolitado < saldo){
             saldo -= valorSolitado;
-            System.out.println(saldo);
+            System.out.println("Novo saldo: "+saldo);
         }
+        else
+            System.out.println("Saldo insuficiente");
     }
 }
