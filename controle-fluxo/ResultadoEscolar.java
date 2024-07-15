58b97b2ebbed54a2f4ded59fbb5574930d6f5208
Gustavo Ferreira <gfmb94@gmail.com>
2024-07-14 23:06:26 -0300

    condicional encadeada

--- controle-fluxo/ResultadoEscolar.java
+++ controle-fluxo/ResultadoEscolar.java
@@ -2,8 +2,10 @@ public class ResultadoEscolar {
     public static void main(String[] args) {
         int nota = 1;
 
-        if(nota >= 7)
+        if (nota >= 7)
             System.out.println("Aprovado");
+        else if (nota >= 5 && nota < 7)
+            System.out.println("Recuperação");
         else
             System.out.println("Reprovado");
     }
