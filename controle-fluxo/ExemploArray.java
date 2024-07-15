70c7b545d084950b9d03ac2fb1b02362d616ed58
Gustavo Ferreira <gfmb94@gmail.com>
2024-07-14 23:49:04 -0300

    for com arrays

---
+++ controle-fluxo/ExemploArray.java
@@ -0,0 +1,9 @@
+public class ExemploArray {
+    public static void main(String[] args) {
+        String alunos [] = {"Felipe","Joana","Maria","Caio"};
+
+        for (int x = 0; x<alunos.length;x++){
+            System.out.println(alunos[x]+" está no índice "+x);
+        }
+    }
+}
