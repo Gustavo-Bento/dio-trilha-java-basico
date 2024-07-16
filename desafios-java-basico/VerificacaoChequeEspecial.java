f93a3c729551f1dffd73b431fcd210ddc86a1524
Gustavo Ferreira <gfmb94@gmail.com>
2024-07-16 13:52:08 -0300

    teste 4 verificação de cheque especial

---
+++ desafios-java-basico/VerificacaoChequeEspecial.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class VerificacaoChequeEspecial {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double saldo = scanner.nextDouble();
+        double saque = scanner.nextDouble();
+
+        // Define um limite para o cheque especial
+        double limiteChequeEspecial = 500;
+
+        // Verifica se o saque ultrapassa o saldo disponível
+        if (saque > saldo) {
+            // TODO: Verificar se o saque ultrapassa o limite do cheque especial
+            if (saque > (limiteChequeEspecial + saldo)) {
+                // Caso contrário, imprimir "Transacao nao realizada. Limite do cheque especial
+                // excedido."
+                System.out.println("Transacao nao realizada. Limite do cheque especial excedido.");
+
+            } else {
+                // Em caso positivo, imprimir "Transacao realizada com sucesso utilizando o
+                // cheque especial."
+                System.out.println("Transacao realizada com sucesso utilizando o cheque especial.");
+            }
+        } else {
+            // TODO: Imprimir "Transação realizada com sucesso."
+            System.out.println("Transacao realizada com sucesso.");
+        }
+
+        // Fechar o scanner para evitar vazamentos de recursos
+        scanner.close();
+    }
+}
