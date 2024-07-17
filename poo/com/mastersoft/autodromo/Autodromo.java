ca71da634837accbacd9af394256e4a53a3effd1
Gustavo Ferreira <gfmb94@gmail.com>
2024-07-17 12:29:24 -0300

    encapsulamento

---
+++ poo/com/mastersoft/autodromo/Autodromo.java
@@ -0,0 +1,8 @@
+package poo.com.mastersoft.autodromo;
+
+public class Autodromo {
+    public static void main(String[] args) {
+        Carro jeep = new Carro();
+        jeep.ligar();
+    }
+}
---
+++ poo/com/mastersoft/autodromo/Carro.java
@@ -0,0 +1,15 @@
+package poo.com.mastersoft.autodromo;
+
+public class Carro {
+    public void ligar(){
+        conferirCambio();
+        verificarCombustivel();
+        System.out.println("Carro ligado");
+    }
+    private void verificarCombustivel(){
+        System.out.println("Combustivel verificado");
+    }
+    private void conferirCambio(){
+        System.out.println("Conferido cambio em P");
+    }
+}
