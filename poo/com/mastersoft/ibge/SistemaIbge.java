f89387990ef5fc1e576e9b04e6d1af0305b44141
Gustavo Ferreira <gfmb94@gmail.com>
2024-07-17 00:41:00 -0300

    enums

---
+++ poo/com/mastersoft/ibge/SistemaIbge.java
@@ -0,0 +1,15 @@
+package poo.com.mastersoft.ibge;
+
+// qualquer classe do sistema poderá obter os objetos de EstadoBrasileiro
+public class SistemaIbge {
+	public static void main(String[] args) {
+		//imprimindo os estados existentes no enum
+		for(EstadoBrasileiro uf: EstadoBrasileiro.values() ) {
+		   System.out.println(uf.getSigla() + "-" + uf.getNomeMaiusculo());
+        }
+		//selecionando um estado a partir das opções disponíveis
+		EstadoBrasileiro eb = EstadoBrasileiro.SAO_PAULO;
+		
+		System.out.println("O estado selecionado foi: " + eb.getIbge());
+	}
+}
