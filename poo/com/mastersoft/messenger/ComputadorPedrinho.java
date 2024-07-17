1068566c6ea6f57415f40c310ab94b0e3c875ab6
Gustavo Ferreira <gfmb94@gmail.com>
2024-07-17 17:41:00 -0300

    encapsulamento

---
+++ poo/com/mastersoft/messenger/ComputadorPedrinho.java
@@ -0,0 +1,11 @@
+package poo.com.mastersoft.messenger;
+
+public class ComputadorPedrinho {
+	public static void main(String[] args) {
+		//abrindo MSN Messenger
+		MSNMessenger msn = new MSNMessenger();
+		
+		msn.enviarMensagem();
+		msn.receberMensagem();
+	}
+}
