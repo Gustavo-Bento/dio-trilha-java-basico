08cf636746a46233cadffa810dd9c0024c244ea0
Gustavo Ferreira <gfmb94@gmail.com>
2024-07-17 18:08:49 -0300

    abstração

--- poo/com/mastersoft/messenger/FacebookMessenger.java
+++ poo/com/mastersoft/messenger/FacebookMessenger.java
@@ -1,5 +1,14 @@
 package poo.com.mastersoft.messenger;
 
 public class FacebookMessenger extends ServicoMensagemInstantanea{
-    
+
+    @Override
+    public void enviarMensagem() {
+        System.out.println("Enviando mensagem pelo Facebook Messenger");  
+    }
+
+    @Override
+    public void receberMensagem() {
+        System.out.println("Recebendo mensagem pelo MSN Messenger");    
+    }    
 }
--- poo/com/mastersoft/messenger/Telegram.java
+++ poo/com/mastersoft/messenger/Telegram.java
@@ -1,5 +1,15 @@
 package poo.com.mastersoft.messenger;
 
 public class Telegram extends ServicoMensagemInstantanea{
+
+    @Override
+    public void enviarMensagem() {
+        System.out.println("Enviando mensagem pelo Telegram");    
+    }
+
+    @Override
+    public void receberMensagem() {
+        System.out.println("Recebendo mensagem pelo Telegram");    
+    }
    
 }
